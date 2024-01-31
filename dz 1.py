46f070754b54bf5b8693a60d24b5fa2ee2aa2c63
Maksim Kaihorodov <makkay289@gmail.com>
2024-01-31 15:40:12 +0200

    1

---
+++ dz 1.py
@@ -0,0 +1,6 @@
+def total_salary(path):
+    with.open('') as path: 
+    
+
+total, average = total_salary("path/to/salary_file.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
