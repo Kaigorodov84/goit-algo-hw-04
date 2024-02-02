01d86272ab222d8c6ac47f6674b7ecbd20dbdca5
Maksim Kaihorodov <makkay289@gmail.com>
2024-02-02 14:51:00 +0200

    comit finish

--- task_2/dz 2.py
+++ task_2/dz 2.py
@@ -1,20 +1,20 @@
 def get_cats_info(path):
-    cats_list = []
+    cats_list = [] #створюємо список 
 
     try:
-        with open(path, 'r') as file:
+        with open(path, 'r') as file: # відкриваємо та читаєм файл
             lines = file.readlines()
 
             for line in lines:
-                cat_info = line.strip().split(',')
+                cat_info = line.strip().split(',') # розділяємо за комою
                 cat_dict = {
                     'id': cat_info[0],
                     'name': cat_info[1],
                     'age': int(cat_info[2])
-                }
-                cats_list.append(cat_dict)
+                }                                 # утворюємо словник з ключами 
+                cats_list.append(cat_dict)        #
 
-    except FileNotFoundError:
+    except FileNotFoundError:                     # обробка винятків
         print(f"File '{path}' not found.")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
